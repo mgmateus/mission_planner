3edfadea69e1a4cc4d68dd324a20e3d092677079
mgmateus <matheus_gonsalvez@outlook.com>
2023-06-23 15:24:13 -0300

    concurrence take_off read_altitude

--- scripts/main.py
+++ scripts/main.py
@@ -23,6 +23,8 @@ if __name__ == "__main__":
             
             con_wait_for_altitude = smach.Concurrence(outcomes=['wait_for_altitude','ready_to_nav'],
                                     default_outcome = 'wait_for_altitude',
+                                    input_keys = ['ready'],
+                                    output_keys = ['ready'],
                                     outcome_map={
                                         'wait_for_altitude' : {'TAKEOFF':'wait_for_autonomous_mode', 'TAKEOFF':'wait_for_altitude', 'READ_ALTITUDE':'wait_for_altitude'},
                                         'ready_to_nav': {'TAKEOFF':'take_off','READ_ALTITUDE':'ready'}
