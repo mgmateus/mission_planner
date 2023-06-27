62b54603728c8f8104188b9847bfa33ce7c43876
mgmateus <matheus_gonsalvez@outlook.com>
2023-06-27 17:01:24 -0300

    Test yaw

---
+++ scripts/teste_yaw.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+"""This is an example of how to do mission planning using BaseController. Do not run this module!"""
+import rospy
+from states import *
+
+if __name__ == "__main__":
+    rospy.init_node("mission_plane_0_node")
+
+    # Create a SMACH state machine
+    sm_mission = smach.StateMachine(outcomes=['mission_finished'])
+
+    with sm_mission:
+        altitude = 0.5
+        position = [[1.2, 0.0, altitude], [1.2, 0.0, 1.5], [0.0, 0.0, 1.5]]
+        yaw = [180]
+
+        sm_its_flying = smach.StateMachine(outcomes=["succeeded"])
+        
+        with sm_its_flying:
+
+            con_wait_for_height = smach.Concurrence(outcomes=['wait_for_height','ready_to_nav'],
+                                    default_outcome = 'wait_for_height',
+                                    outcome_map={
+                                        'ready_to_nav': {'TAKEOFF':'take_off','READ_HEIGHT':'ready'}
+                                    })
+            
+            con_wait_for_yaw_1 = smach.Concurrence(outcomes=['wait_for_yaw_1','ready_to_nav_1'],
+                                    default_outcome = 'wait_for_yaw_1',
+                                    outcome_map={
+                                        'ready_to_nav_1': {'YAW_1':'turned','READ_YAW_1':'ready_yaw'}
+                                    })
+            
+            smach.StateMachine.add("ARMED", Armed(),
+                                   transitions={
+                                       "wait_for_arming":"ARMED",
+                                        "armed" : "WAIT_FOR_HEIGHT"
+                                    })
+            
+            with con_wait_for_height:
+                smach.Concurrence.add('READ_HEIGHT', RangeFinderCheck(altitude))
+                smach.Concurrence.add('TAKEOFF', TakeOff(altitude))
+            
+            smach.StateMachine.add("WAIT_FOR_HEIGHT", con_wait_for_height,
+                                   transitions={
+                                        'wait_for_height' : "WAIT_FOR_HEIGHT",
+                                        'ready_to_nav' : "WAIT_FOR_YAW_1"
+                                    })
+            
+            with con_wait_for_yaw_1:
+                smach.Concurrence.add('READ_YAW_1', YawCheck(target_yaw=yaw[0]))
+                smach.Concurrence.add('YAW_1', Yaw(yaw[0]))
+
+            
+            smach.StateMachine.add("WAIT_FOR_YAW_1", con_wait_for_yaw_1,
+                                   transitions={
+                                        'wait_for_yaw_1' : "WAIT_FOR_YAW_1",
+                                        'ready_to_nav_1' : "LAND"
+                                    })
+            
+            smach.StateMachine.add("LAND", Land(),
+                                   transitions={
+                                        "land" : "succeeded"
+                                    })
+            
+        smach.StateMachine.add("ITS_FLING", sm_its_flying,
+                                    transitions={
+                                            "succeeded" : "mission_finished"
+                                        })
+        
+    
+    # Execute SMACH plan
+    outcome = sm_mission.execute()
