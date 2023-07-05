e636699149e19630172469c8e6d3d1f22b2f019f
mgmateus <matheus_gonsalvez@outlook.com>
2023-07-05 17:56:15 -0300

    Optmizer state machines

--- scripts/main.py
+++ scripts/main.py
@@ -9,8 +9,8 @@ if __name__ == "__main__":
     # Create a SMACH state machine
     height = 0.6
     target = [0.25, 0.0, 0.6]
-    path = [[1.2, 0.0, height], [1.2, 0.0, 1.0], [0.0, 0.0, 1.0]]
-    sm_mission = check_waypoints_navigation(height, target)
+    waypoints = [[1.2, 0.0, height], [1.2, 0.0, 1.0], [0.0, 0.0, 1.0]]
+    sm_mission = check_waypoints_navigation(height, waypoints)
     
     # Execute SMACH plan
     outcome = sm_mission.execute()
