88d7850b77987cb3c9e09171e06d058847aa1294
mgmateus <matheus_gonsalvez@outlook.com>
2023-07-05 16:39:32 -0300

    Optmizer state machines

--- scripts/machines.py
+++ scripts/machines.py
@@ -1,5 +1,6 @@
 import rospy
 import smach
+from smach import CBState
 from typing import *
 from check_states import *
 from states import *
@@ -59,7 +60,7 @@ def pilot_flight(target_height : float) -> smach.StateMachine:
 
         
         
-        smach.StateMachine.add("FLING", sm_mission_start,
+        smach.StateMachine.add("FLYING", sm_mission_start,
                                    transitions={
                                         "succeeded" : "LAND"
                                     })
@@ -79,14 +80,14 @@ def goto(target_height : float, position : List) -> smach.StateMachine:
     with sm:
         sm_mission_start = mission_start(target_height)
 
-        smach.StateMachine.add("FLING", sm_mission_start,
+        smach.StateMachine.add("FLYING", sm_mission_start,
                                    transitions={
                                         "succeeded" : "GO_TO"
                                     })
         
         smach.StateMachine.add("GO_TO", Navigate(position=position),
                                    transitions={
-                                        'wait_for_position' : "GO_TO"
+                                        'wait_for_position' : "CHECK_POSITION"
                                     })
         
         smach.StateMachine.add("CHECK_POSITION", PositionCheck(target_position=position),
@@ -108,7 +109,7 @@ def test_nav(target_height : float, position : List) -> smach.StateMachine:
     with sm:
         sm_mission_start = mission_start(target_height)
 
-        smach.StateMachine.add("FLING", sm_mission_start,
+        smach.StateMachine.add("FLYING", sm_mission_start,
                                    transitions={
                                         "succeeded" : "GO_TO"
                                     })
@@ -137,3 +138,79 @@ def test_nav(target_height : float, position : List) -> smach.StateMachine:
                                })
         
     return sm
+
+def test_altitude_ajust(target_height : float, path : List) -> smach.StateMachine:
+
+    sm = smach.StateMachine(outcomes=["succeeded"])
+    sm.userdata.path = path
+
+    with sm:
+        sm_mission_start = mission_start(target_height)
+        
+        smach.StateMachine.add("FLYING", sm_mission_start,
+                                transitions={
+                                    "succeeded" : "GO_TO_PATH"
+                                })
+        
+        it_goto = smach.Iterator(outcomes = ['succeeded'],
+                                    input_keys = ['path'],
+                                    output_keys=['position'],
+                                    it = lambda: range(0, len(path)),
+                                    it_label = 'i',
+                                    exhausted_outcome = 'succeeded')
+        
+        with it_goto:
+            container_sm = smach.StateMachine(outcomes = ['succeeded','continue'],
+                                            input_keys = ['path'],
+                                            output_keys = ['position'])
+            
+            with container_sm:
+
+                @smach.cb_interface(input_keys=['path'],
+                                    output_keys=['position'], 
+                                    outcomes=['succeeded'])
+                
+                def pop_position_cb(ud):
+                    ud.position = ud.path.pop(0)
+                    return 'succeeded'
+
+                smach.StateMachine.add('POP_POSITION', CBState(pop_position_cb), 
+                                {'succeeded':'GO_TO'})
+                
+                sm_goto = goto(target_height, sm.userdata.position)
+                
+                smach.StateMachine.add("GO_TO", sm_goto,
+                                    transitions={
+                                        "succeeded" : "CHECK_PATH"
+                                    })
+                
+                @smach.cb_interface(input_keys=['path'],
+                                    output_keys=['path'], 
+                                    outcomes=['succeeded', 'continue'])
+                
+                def finished_path_cb(ud):
+                    return 'succeeded' if len(ud.path) == 0 else 'continue'
+                
+                smach.StateMachine.add('CHECK_PATH', CBState(finished_path_cb), 
+                                {'succeeded':'succeeded',
+                                'continue':'continue'
+                                })
+
+            
+            #close container_sm
+            Iterator.set_contained_state('CONTAINER_STATE', 
+                                        container_sm, 
+                                        loop_outcomes=['continue'])
+        #close it_goto        
+        smach.StateMachine.add("GO_TO_PATH", it_goto,
+                                transitions={
+                                    "succeeded" : "LAND"
+                                })
+        
+        smach.StateMachine.add("LAND",
+                               Land(),
+                               transitions={
+                                   "land": "succeeded"
+                               })
+        
+    return sm 
--- scripts/main.py
+++ scripts/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """This is an example of how to do mission planning using BaseController. Do not run this module!"""
 import rospy
-from machines import pilot_flight, test_nav
+from machines import pilot_flight, test_nav, test_altitude_ajust
 
 if __name__ == "__main__":
     rospy.init_node("mission_plane_0_node")
@@ -9,7 +9,8 @@ if __name__ == "__main__":
     # Create a SMACH state machine
     height = 0.6
     target = [0.25, 0.0, 0.6]
-    sm_mission = test_nav(height, target)
+    path = [[1.2, 0.0, height], [1.2, 0.0, 1.0], [0.0, 0.0, 1.0]]
+    sm_mission = test_altitude_ajust(height, target)
     
     # Execute SMACH plan
     outcome = sm_mission.execute()
