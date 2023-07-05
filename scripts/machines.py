3cb9bdfb79cc3e11e05a38b9e9a0352d30a1379a
mgmateus <matheus_gonsalvez@outlook.com>
2023-07-05 19:12:07 -0300

    Optmizer state machines

--- scripts/machines.py
+++ scripts/machines.py
@@ -91,13 +91,13 @@ def goto_waypoint(waypoint : List) -> smach.StateMachine:
         smach.StateMachine.add("GO_TO", WaypointNavigate(),
                                 input_keys=["waypoint"],
                                 transitions={
-                                    'wait_for_position' : "CHECK_POSITION"
+                                    'wait_for_waypoint' : "CHECK_POSITION"
                                 })
         
         smach.StateMachine.add("CHECK_POSITION", WaypointCheck(),
                                 input_keys=["waypoint"],
                                 transitions={
-                                    'wait_for_position' : "CHECK_POSITION",
+                                    'wait_for_waypoint' : "CHECK_POSITION",
                                     'ready' : "succeeded"
                                 })
 
