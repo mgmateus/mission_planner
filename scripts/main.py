3c4c61a703cb016e73d55ca1ad10e6b253d7b670
mgmateus <matheus_gonsalvez@outlook.com>
2023-07-05 13:22:45 -0300

    Optmizer state machines

--- scripts/main.py
+++ scripts/main.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 """This is an example of how to do mission planning using BaseController. Do not run this module!"""
 import rospy
-from machines import pilot_flight
+from machines import pilot_flight, test_nav
 
 if __name__ == "__main__":
     rospy.init_node("mission_plane_0_node")
 
     # Create a SMACH state machine
-    sm_mission = pilot_flight(1.0)
+    sm_mission = test_nav(1.0, [0.1, 0.0, 0.8])
     
     # Execute SMACH plan
     outcome = sm_mission.execute()
